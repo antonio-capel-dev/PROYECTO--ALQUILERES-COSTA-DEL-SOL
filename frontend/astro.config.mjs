8f6059afbfb3d7e5e4a6a5f90880ba38cb9a28db
AntonioCapelRamirez1991 <antoniomiguelcapelramirez@gmail.com>
2026-02-14 15:13:21 +0100

    arreglando el footer que estaba roto y seo finalizado

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  site: "https://alquileres-costadelsol.com",
+  integrations: [sitemap()],
 });
